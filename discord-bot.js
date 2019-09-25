ea72483a0f59d072386a8998cf38bc4106de48be
Djobbo Maïga <corehallabusiness@gmail.com>
2019-09-25 08:47:49 +0000

    deleted gql

--- discord-bot.js
+++ discord-bot.js
@@ -1,16 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const ApolloBoost = require('apollo-boost');
-const fetch = require('node-fetch');
-const ApolloClient = ApolloBoost.default;
-const { gql } = require('apollo-boost');
-
-const apolloClient = new ApolloClient({
-    uri: 'http://localhost:31199',
-    fetch: fetch
-});
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setStatus("dnd");
@@ -41,42 +31,6 @@ client.on('message', msg => {
 
 const register = (name, id) => {
 
-    const GET_USERS = gql`
-    query FetchUsers($discordID: String!) {
-        users(discordID: $discordID) {
-            id
-            name
-            discordID
-        }
-    }
-    `
-
-    const CREATE_USER = gql`
-    mutation CreateUser($name: String!, $discordID: String!) {
-        userCreate(name: $name, discordID: $discordID) {
-            id
-            name
-            discordID
-        }
-    }
-    `
-    apolloClient.query({
-        query: GET_USERS,
-        variables: {
-            discordID: id
-        }
-    }).then(res => {
-        console.log(res.data);
-        if (res.data.users.length > 0)
-            return;
-        apolloClient.mutate({
-            mutation: CREATE_USER,
-            variables: {
-                name: name,
-                discordID: id
-            }
-        }).then(console.log);
-    })
 }
 
 client.login(process.env.DISCORD_BOT_TOKEN);
